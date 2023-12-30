d801d7739b3f078d233138c8e357b099abc370a9
moneshongithub <monesh.lodhi123@gmail.com>
2023-12-30 21:30:27 +0530

    sdvd

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -6,7 +6,7 @@ const AddItem = () => {
     <div className='row kg-row'>
         <div className='col-6'> buy milk</div>
         <div className='col-4'> 4/10/2000</div>
-        <div className='col-2'><button type='button' className='btn btn-danger'> Delete</button></div>
+        <div className='col-2'><button type='button' className='btn btn-danger'> DelJsbckjaseteee</button></div>
     </div>
     </div>
   )
